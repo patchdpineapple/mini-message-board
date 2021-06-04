7efbf25e0d68b61933d13009646e49d0097c7acb
patchdpineapple <seanpatrick.penaranda@gmail.com>
2021-06-04 20:37:18 +0800

    Completed routes and templates for index and form. added bootstrap and custom styling.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,32 @@
 var express = require('express');
 var router = express.Router();
 
+// Messages
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date()
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date()
+  }
+];
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Mini Message Board', messages: messages });
+});
+
+router.get('/new', function(req, res, next) {
+  res.render('form', { title: 'Mini Message Board', messages: messages });
+});
+
+router.post('/new', function(req, res, next) {
+  messages.push({text: req.body.message, user: req.body.user, added: new Date()});
+  res.redirect("/");
 });
 
 module.exports = router;
